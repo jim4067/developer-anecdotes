387580b6b7d1c6c84f7371becb3b8c5824002520
jim4067 <jimmyimpulse2@gmail.com>
2021-09-24 21:20:17 +0300

    set up the app struct and gtk widgets

--- src/main.rs
+++ src/main.rs
@@ -1,150 +1,95 @@
-extern crate gtk;
-extern crate rand;
+#[macro_use]
+extern crate cascade;
 
-use gtk::*;
-use rand::{thread_rng, Rng};
+use async_channel::Sender;
+use gtk::prelude::*;
+use std::future::Future;
 use std::process;
-use std::sync::{
-    atomic::{AtomicUsize, Ordering},
-    Arc,
-};
 
-const ANECDOTES_LIST : [&str; 6] = [
-    "If it hurts, do it more often",
-    "Adding manpower to a late software project makes it later!",
-    "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
-    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
-    "Premature optimization is the root of all evil.",
-    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it."
-];
+enum Event {
+    Clicked,
+}
 
 fn main() {
+    glib::set_program_name("developer anecdotes".into());
+    glib::set_application_name("developer anecdotes");
+
     if gtk::init().is_err() {
-        eprintln!("failed to init the GTK app");
+        eprintln!("Error ! Failed to start gtk application");
         process::exit(1);
     }
-    //setting the atomically referenced var
-    let random = gen_random();
 
-    let index = Arc::new(AnecdoteComponent::new(random));
+    let (tx, rx) = async_channel::unbounded();
 
-    let app = App::new(&index);
+    let event_handler = async move {
+        while let Ok(message) = rx.recv().await {
+            match message {
+                Event::Clicked => {}
+            }
+        }
+    };
+}
 
-    {
-        let index_label = app.content.index_label.clone();
-        let anecdote = app.content.anecdote.clone();
-        app.content.next.clone().connect_clicked(move |_| {
-            let new_index = gen_random();
-            anecdote.set_label(ANECDOTES_LIST[new_index]);
-            index_label.set_label(format!("{} / 6", new_index.to_string().as_str()).as_str());
-        });
-    }
-
-    app.window.show_all();
-    gtk::main();
+pub fn spawn<F>(future: F)
+where
+    F: Future<Output = ()> + 'static,
+{
+    glib::MainContext::default().spawn_local(future)
 }
 
 struct App {
-    pub header: Header,
-    pub window: Window,
-    pub content: Content,
+    pub anecdote: gtk::Label,
+    pub button: gtk::Button,
+    // pub window: gtk::Window,
 }
-
-struct Header {
-    pub header_container: HeaderBar,
-}
-
-struct Content {
-    pub anecdote: Label,
-    pub container: Box,
-    index_label: Label,
-    pub next: Button,
-}
-
-struct AnecdoteComponent(AtomicUsize);
 
 impl App {
-    fn new(index: &AnecdoteComponent) -> App {
-        let window = Window::new(WindowType::Toplevel);
-        let header = Header::new();
-        let content = Content::new(index);
+    pub fn new(tx: Sender<Event>, anecdote: &str) -> Self {
+        //text widget to display the anecdote down under
+        let anecdote = gtk::Label::new(anecdote.into());
 
-        window.set_title("developer anecdotes");
-        window.set_default_size(550, 250);
-        window.set_position(WindowPosition::CenterAlways);
-        Window::set_default_icon_name("developer anecdotes");
+        let button = cascade! {
+            gtk::Button::with_label("next");
+            ..get_style_context().add_class("destructive-action");
+            ..set_border_width(4);
+            ..connect_clicked(move |_|{
+                let tx = tx.clone();
+                spawn(async move {
+                    let _ = tx.send(Event::Clicked).await;
+                });
+            });
+        };
 
-        //add the content box to the window
-        window.add(&content.container);
+        //will stitch up everything
+        let container = cascade! {
+            gtk::Box::new(gtk::Orientation::Vertical, 0);
+            ..add(&anecdote);
+            ..add(&button);
+            ..show_all();
+        };
 
-        window.connect_delete_event(move |_, _| {
-            main_quit();
-            Inhibit(false)
-        });
+        //a touch of gtk magic to make the window appear
+        let _window = cascade! {
+            gtk::Window::new(gtk::WindowType::Toplevel);
+            ..add(&container);
+            ..set_title("developer anecdotes");
+            ..set_default_size(400, 250);
+            ..set_position(gtk::WindowPosition::CenterAlways);
+            ..connect_delete_event(move |_,_|{
+                gtk::main_quit();
+                gtk::Inhibit(false)
+            });
+            ..show_all();
+        };
 
-        App {
-            window,
-            header,
-            content,
-        }
+        gtk::Window::set_default_icon_name("jaw dropping icon name");
+
+        Self { anecdote, button }
     }
 }
 
-impl Header {
-    fn new() -> Header {
-        let header_container = HeaderBar::new();
+// struct Content {
+//     anecdote: gtk::
+// }
 
-        // container.set_title(Some("developer anecdotes"));
-        header_container.set_show_close_button(true);
-
-        Header { header_container }
-    }
-}
-
-impl Content {
-    fn new(index: &AnecdoteComponent) -> Content {
-        let container = Box::new(Orientation::Vertical, 0);
-
-        let anecdote_container = Box::new(Orientation::Horizontal, 0);
-        let index_str = index.get_index();
-        let index_label = format!("{} / 6 ->", index_str).to_string(); //concatenating in rust is not a walk in the park/ do not also get fond of the compiler making assumptions of your types
-        let index_label = Label::new(Some(&index_label));
-        let index = index.get_index();
-        let anecdote = Label::new(Some(ANECDOTES_LIST[index]));
-
-        index_label.set_halign(Align::Start);
-        anecdote.set_halign(Align::Center);
-        anecdote_container.pack_start(&index_label, false, false, 5);
-        anecdote_container.pack_start(&anecdote, true, true, 5);
-
-        // let button_container = Box::new(Orientation::Horizontal, 0);
-        let next = Button::with_label("next");
-        next.get_style_context().add_class("destructive-action");
-        anecdote_container.pack_end(&next, true, true, 5);
-
-        container.pack_start(&anecdote_container, true, false, 0);
-
-        Content {
-            anecdote,
-            container,
-            index_label,
-            next,
-        }
-    }
-}
-
-impl AnecdoteComponent {
-    fn new(index: usize) -> AnecdoteComponent {
-        AnecdoteComponent(AtomicUsize::new(index))
-    }
-    fn get_index(&self) -> usize {
-        self.0.load(Ordering::SeqCst)
-    }
-}
-
-//the function for generating the random numbers
-fn gen_random() -> usize {
-    let mut rng = thread_rng();
-    let rand_index = rng.gen_range(0..6);
-    rand_index
-}
+//instead create a function to randomly select an anecdote to spit out once selected
